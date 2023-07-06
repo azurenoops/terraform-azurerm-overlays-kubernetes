95817232ff6c0506f2accb2575c55ec5e620c533
John Spinella <jrspinella@users.noreply.github.com>
2023-07-06 12:19:37 -0400

    updated aks id

--- resources.kubernetes.identity.tf
+++ resources.kubernetes.identity.tf
@@ -1,14 +1,35 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
 
-resource "azurerm_user_assigned_identity" "aks_identity" {
-  resource_group_name = local.resource_group_name
-  location            = local.location
+resource "azurerm_user_assigned_identity" "aks" {
+  count = (var.identity_type == "UserAssigned" && var.user_assigned_identity == null ? 1 : 0)
+
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  name                = local.user_assigned_identity_name
+
   tags                = var.add_tags
-
-  name = "${var.name}Identity"
 
   lifecycle {
     ignore_changes = [
       tags
     ]
   }
+}
+
+resource "azurerm_role_assignment" "subnet_network_contributor" {
+  for_each = (var.virtual_network == null ? {} : (var.configure_network_role ? var.virtual_network.subnets : {}))
+
+  scope                = each.value.id
+  role_definition_name = "Network Contributor"
+  principal_id         = local.aks_identity_id
+}
+
+resource "azurerm_role_assignment" "route_table_network_contributor" {
+  count = (var.virtual_network == null ? 0 : 1)
+
+  scope                = var.virtual_network.route_table_id
+  role_definition_name = "Network Contributor"
+  principal_id = (var.user_assigned_identity == null ? azurerm_user_assigned_identity.aks.0.principal_id :
+  var.user_assigned_identity.principal_id)
 }
