89ef61574d88cabedf1f41d20a9d83e6ebabd55a
John Spinella <jrspinella@users.noreply.github.com>
2023-07-06 12:20:01 -0400

    updated banner

--- resources.kubernetes.roles.tf
+++ resources.kubernetes.roles.tf
@@ -1,3 +1,5 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
 
 resource "azurerm_role_assignment" "network_contributor" {
   scope                = local.resource_group_name
