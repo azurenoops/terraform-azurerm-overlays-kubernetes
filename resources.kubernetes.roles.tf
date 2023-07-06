0ce8248f5135da4a598f62b4f6d93afad178a5da
John Spinella <jrspinella@users.noreply.github.com>
2023-07-06 19:55:42 -0400

    update roles

--- resources.kubernetes.roles.tf
+++ resources.kubernetes.roles.tf
@@ -8,9 +8,10 @@ resource "azurerm_role_assignment" "network_contributor" {
   skip_service_principal_aad_check = true
 }
 
-resource "azurerm_role_assignment" "acr_pull" {
+resource "azurerm_role_assignment" "aks_acr_pull_allowed" {
+  for_each = toset(var.acr_pull_access)
+
+  principal_id         = azurerm_user_assigned_identity.aks_user_assigned_identity.principal_id
+  scope                = each.value
   role_definition_name = "AcrPull"
-  scope                = module.container_registry.id
-  principal_id         = module.aks_cluster.kubelet_identity_object_id
-  skip_service_principal_aad_check = true
 }
